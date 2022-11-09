a28c8454cda431b3a22d6f34f73f3f4a89cd8785
Alef Computers <dimarik27092001@gmail.com>
2022-11-09 16:47:54 +0300

    Add new tests.

--- TAP/Program.cs
+++ TAP/Program.cs
@@ -2,6 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
 
 namespace TAP
 {
@@ -42,6 +45,36 @@ namespace TAP
             return;
         }
 
+        private async static Task TestAwaitMultiple()
+        {
+            Stopwatch timer = new();
+            timer.Start();
+            for (int i = 0; i < 5; i++)
+                await Task.Delay(200);
+            timer.Stop();
+            Console.WriteLine($"Текущий поток: {Thread.CurrentThread.ManagedThreadId}. Время выполнения: {timer.Elapsed}");
+            return;
+        }
+
+        private async static Task TestAwaitMultiple2()
+        {
+            Stopwatch timer = new();
+            HashSet<Task> Tasks = new();
+            timer.Start();
+            for (int i = 0; i < 5; i++)
+                Tasks.Append(Task.Delay(200));
+            Task.WaitAll(Tasks.ToArray());
+            timer.Stop();
+            Console.WriteLine($"Текущий поток: {Thread.CurrentThread.ManagedThreadId}. Время выполнения: {timer.Elapsed}");
+            return;
+        }
+
+        private async static Task TestRun()
+        {
+            Task.Run(() => Console.WriteLine($"Текущий поток: {Thread.CurrentThread.ManagedThreadId}"));
+            return;
+        }
+
         static void Main(string[] args)
         {
             Start();
@@ -51,6 +84,9 @@ namespace TAP
                 //TestAsync();
                 //TestAwait();
                 //TestWait();
+                //TestAwaitMultiple();
+                //TestAwaitMultiple2();
+                TestRun();
             }
             Console.WriteLine("Работа основного потока завершена...");
             Console.ReadKey();
